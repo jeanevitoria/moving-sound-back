d300be16d8f000b1a7c50dbe2f543354a87cf4c5
Vitória <jeanevitoria994@gmail.com>
2025-08-06 00:41:07 -0300

    feat: requirements.txt.

--- backend/settings.py
+++ backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("CLIENT_SECRET");
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -96,8 +96,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
     }
 }
 
